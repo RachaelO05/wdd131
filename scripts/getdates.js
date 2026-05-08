cb042a571253a6fc2f93ec8b1f8b21ef6342e592
RachaelO05 <romoore@byupathway.edu>
2026-05-08 14:29:01 +0200

    Home page javascript additions

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+const currentyear = document.querySelector("#currentyear");
+const lastModified = document.getElementById("lastModified").innerHTML =document.lastModified;
+
+const today = new Date();
+
+currentyear.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;

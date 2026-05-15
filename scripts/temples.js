8a3bf6bf222837e9f5df159a2dc9ac9f39c8443d
RachaelO05 <romoore@byupathway.edu>
2026-05-15 13:51:55 +0200

    temple page final

---
+++ scripts/temples.js
@@ -0,0 +1,29 @@
+const menuButton = document.createElement("button");
+const header = document.querySelector("header");
+const span = document.querySelector("span")
+
+menuButton.textContent = '☰';
+menuButton.style.backgroundColor = '#03c28f'
+menuButton.style.border = 'none';
+menuButton.style.color = '#fff';
+menuButton.style.fontSize = '150%'
+
+header.append(menuButton);
+
+const nav = document.querySelector("nav");
+
+menuButton.addEventListener('click', () => {
+    nav.classList.toggle('open');
+
+    if (nav.classList.contains('open')) {
+        menuButton.textContent = '✖';
+        span.style.display = "none";
+    }
+    else {
+        menuButton.textContent = '☰';
+        span.style.display = "block";
+    }
+
+});
+
+

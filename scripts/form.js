de2dd10d4e7a8c96d58b474493ec49fa4e3afed9
RachaelO05 <romoore@byupathway.edu>
2026-06-04 16:06:40 +0200

    form page edit

--- scripts/form.js
+++ scripts/form.js
@@ -29,7 +29,7 @@ const products = [
 const currentPage = window.location.pathname;
 const productName = document.getElementById("productName");
 
-if (currentPage.includes("form.html") || currentPage.includes("form")) {
+if (currentPage.includes("form.html")) {
     products.forEach((product) => {
         option = document.createElement("option");
 
